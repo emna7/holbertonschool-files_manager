7004aaf76a1d5a97cb00d2dfbe73ef0bc0150bad
Edin93 <il_matador@hotmail.fr>
2021-01-27 15:35:10 +0100

    task 1

--- utils/redis.js
+++ utils/redis.js
@@ -4,13 +4,19 @@ import { promisify } from 'util';
 class RedisClient {
   constructor() {
     this.client = redis.createClient();
+    this.client.on("error", function(error) {
+      console.error(error);
+    });
     this.clientGet = promisify(this.client.get).bind(this.client);
     this.clientSet = promisify(this.client.set).bind(this.client);
     this.clientDel = promisify(this.client.del).bind(this.client);
   }
 
   isAlive() {
-    return this.client.connected;
+    if (this.client.ping) {
+      return true;
+    }
+    return false;
   }
 
   async get(key) {
@@ -26,5 +32,6 @@ class RedisClient {
     await this.clientDel(key);
   }
 }
+
 const redisClient = new RedisClient();
 export default redisClient;

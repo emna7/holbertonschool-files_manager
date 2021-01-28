03af3ba261cd77562cf8d96fa53346bdb442baa0
Edin93 <il_matador@hotmail.fr>
2021-01-28 16:56:49 +0100

    task 2 update

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -4,17 +4,23 @@ import dbClient from '../utils/db';
 export function getStatus(req, res) {
   if (redisClient.isAlive() && dbClient.isAlive()) {
     res.status(200).send({ redis: true, db: true });
+  } else {
+    res.send({ redis: false, db: false });
   }
 }
 
-export function getStats(req, res) {
-  const nbUsers = dbClient.nbUsers();
-  const nbFiles = dbClient.nbFiles();
+export async function getStats(req, res) {
+  const nbUsers = await dbClient.nbUsers();
+  const nbFiles = await dbClient.nbFiles();
   const result = {
     users: nbUsers,
     files: nbFiles,
   };
+
+  // console.log("----------------------------------------");
+
+  // console.log(typeof(nbUsers));
+
+  // console.log("----------------------------------------");
   res.status(200).send(result);
 }
-
-// export default { getStatus, getStats };

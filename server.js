da652ee7f9bc766a8c246575cbe00efa4442915f
Edin93 <il_matador@hotmail.fr>
2021-01-29 16:03:03 +0100

    test express.json instead of bodyparser

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 const routes = require('./routes');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
+app.use(express.json());
 app.use(routes);
 app.listen(port, () => console.log(`Server running on port ${port}`));

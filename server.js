47c7d09edba0c88e76dab45315b7dff6cfe3defc
Edin93 <il_matador@hotmail.fr>
2021-01-29 15:53:10 +0100

    remove bodyparser

--- server.js
+++ server.js
@@ -1,14 +1,10 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 const routes = require('./routes');
 
 const app = express();
-
 const port = process.env.PORT || 5000;
 
-// app.use(routes);
-
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 app.use(routes);
-
 app.listen(port, () => console.log(`Server running on port ${port}`));

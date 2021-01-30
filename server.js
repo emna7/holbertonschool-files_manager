2f720ee30d8aad18b6a3bab39fd3da78ea5f4de8
Edin93 <il_matador@hotmail.fr>
2021-01-30 11:46:53 +0100

    reuse express.json()

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const routes = require('./routes');
 const app = express();
 const port = process.env.PORT || 5000;
 
-// app.use(express.json());
+app.use(express.json());
 app.use(routes);
 app.listen(port, () => console.log(`Server running on port ${port}`));
